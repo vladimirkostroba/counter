7711fa919c4eb093f18f43ca187e25a96862b22e
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2023-12-29 06:01:14 +0100

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+const refs = {
+    start:document.querySelector('.js-start'),
+    stop:document.querySelector('.js-stop'),
+}
+
+let isActive = null;
+
+// //
+
+refs.start.addEventListener('click',startToChangeColor );
+refs.stop.addEventListener('click',stopToChangeColor);
+
+// //
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+// //
+
+function startToChangeColor(){
+    if(isActive){
+        console.log('no');
+        return
+    }
+
+   intervalId =  setInterval(() => {
+        isActive = true;
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+} 
+
+function stopToChangeColor(){
+    clearInterval(intervalId);
+    isActive = null;
+}
+
+
+
 
