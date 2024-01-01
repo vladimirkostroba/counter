abe594f8d4d92fd56826ec41f401ba9e9c82e53b
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2024-01-01 14:58:50 +0100

    commit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,11 +1,19 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
     input:document.querySelector('#datetime-picker'),
     start:document.querySelector('#start'),
+    days:document.querySelector('[data-days]'),
+    hours:document.querySelector('[data-hours]'),
+    minutes:document.querySelector('[data-minutes]'),
+    seconds:document.querySelector('[data-seconds]'),
 }
 
+const message = 'Please choose a date in the future';
+
+// //
 
 const options = {
     enableTime: true,
@@ -14,7 +22,7 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
     if(selectedDates[0].getTime() < Date.now()){
-        alert('no')
+        Notify.info(message)
         refs.start.disabled = true;
         return
     }
@@ -25,18 +33,34 @@ const options = {
 
   flatpickr('#datetime-picker',options)
 
-  refs.start.addEventListener('click', function (e) {
-    const selectedDates = new Date(refs.input.value);
+  // //
 
-    console.log(selectedDates);
+  refs.start.addEventListener('click', function (e) {
+    const selectedDates = new Date(refs.input.value).getTime();
+
+
+    setInterval(() => {
+      const dateNow = Date.now()
+
+      const timeLeft = selectedDates - dateNow;
+
+      markapHandler(timeLeft)
+    }, 1000);
+
   });
 
+  // //
+
+  function markapHandler(deltaTime){
+        refs.days.textContent = Math.floor(deltaTime / (1000 * 60 * 60 * 24));
+        refs.hours.textContent = Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        refs.minutes.textContent = Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60));
+        refs.seconds.textContent = Math.floor((deltaTime % (1000 * 60)) / 1000);
+}
 
 
 
-// const date = new Date()
 
-// console.log(date.getTime());
 
 
 
